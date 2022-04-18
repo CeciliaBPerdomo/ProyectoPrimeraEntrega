bd395206a16ae2f131543798cf6fa3e2128124a9
Cecilia <cecilia.perdomo@gmail.com>
2022-04-18 19:40:32 -0300

    3

--- routes.js
+++ routes.js
@@ -1,14 +1,18 @@
-const express = require ('express')
+//const express = require ('express')
+import express from 'express'
 const routerProductos = express.Router()
 
-const container = require('./contenedor');
+//const container = require('./contenedor');
 
 const prodDisponibles = require('./testproductos')
-
-
 
 /* Mostrar productos */
 routerProductos.get('/', async (req, res) => {
     console.log(await prodDisponibles())
     res.send(await prodDisponibles())
 })
+
+export default routerProductos
+
+/* Contenedor */
+//module.exports = container;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ npm install express
 const express = require ('express')
 const routes = require('./routes')
 
+/*import express from 'express'
+import routes from './routes'*/
+
 const app = express()
 
 app.use(express.json());
@@ -21,7 +24,3 @@ const srv = app.listen(PORT, () => {
     console.log(`Escuchando en el puerto ${srv.address().port}`)
 })
 srv.on('error', error => console.log(`Error en el servidor ${error}`))
-
-
-/* Contenedor */
-module.exports = container;
