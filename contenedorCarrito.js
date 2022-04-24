de99b9a8abaa302ca37ac99dd8bc517fdd2e6663
Cecilia <cecilia.perdomo@gmail.com>
2022-04-23 22:48:18 -0300

    commit 18

--- contenedorCarrito.js
+++ contenedorCarrito.js
@@ -6,11 +6,39 @@ class ContainerCarrito {
 
     crearCarrito(){
         try{
-            const carritos = []            
+            const carritos = []      
+            const carrito = {
+                id: 1,
+                timestamp: Date.now(),
+                productos: []
+            }
+            carritos.push(carrito) 
+            fs.writeFileSync(this.filename, JSON.stringify(carritos))
+            console.log(carrito.id)     
         } catch(error){
             throw new Error(error)
         }
     }
+
+    // Mostrar carrito
+    async leer(){
+        try{
+            let contenido = await fs.promises.readFile(this.filename, 'utf-8')
+            console.log(contenido)
+            return JSON.parse(contenido)
+        }catch(error){
+            throw new Error(error)
+        }    
+    }
+
+    /*
+    async mostrarPorId(id){
+        try {
+
+        } catch(error) {
+            throw new Error(error)
+        }
+    }*/
 
 }
 
--- routesCarrito.js
+++ routesCarrito.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const routerCarrito = express.Router()
 
-/* Muestra el carrito */
-routerCarrito.post('/', async(req, res) => {
+const container = require('./contenedorCarrito')
+const datos = new container('./carrito.txt')
 
+/* Inicializa el carrito, con id 1
+ datos.crearCarrito() */
+
+/* Muestra el carrito */
+routerCarrito.get('/', async(req, res) => {
+    let carrito = await datos.leer()
+    res.render('carrito', { carrito })
 })
 
 /* Guarda productos en el carrito */
@@ -13,7 +20,11 @@ routerCarrito.post('/:id/productos', async(req, res) => {
 
 /* Elige el carrito según el id*/ 
 routerCarrito.get('/:id', async(req, res) => {
-
+    const { id } = req.params
+    let carrito = await datos.leer()
+    let carritoId = carrito.filter(carrito => carrito.id === parseInt(id))
+    console.log(carritoId)
+    res.send(carritoId)
 })
 
 /*Borra un producto del carrito */
@@ -22,6 +33,11 @@ routerCarrito.delete('/:id/productos/:idprod', async(req, res) => {
 })
 
 routerCarrito.put('/:id/productos/:idprod', async(req, res) => {
+
+})
+
+// Borra el carrito
+routerCarrito.delete('/borrar/:id', async(req, res) => {
 
 })
 
